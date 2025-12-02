d9ae11badde72e5798db02a4694fc0ff39b1443a
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-12-02 09:03:09 -0500

    Add logic to handle CLI arguments.

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	argCount := len(os.Args)
+	if argCount > 2 {
+		log.Println("too many arguments provided")
+		os.Exit(1)
+	} else if argCount < 2 {
+		log.Println("no website provided")
+		os.Exit(1)
+	}
+
+	baseURL := os.Args[1]
+	log.Printf("starting crawl of: %s\n", baseURL)
 }
